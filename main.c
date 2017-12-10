07547638e6cf8dcc0737b90bc0836d6ebe789b42
MChen <mfchen@outlook.com>
2017-12-10 13:28:41 -0500

    add test

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
+#include <stdlib.h>
 #include "tpm.h"
+
+void usage()
+{
+	printf("usage:\ttpm <log file path>\n");
+}
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	if(argc <= 1){
+		usage();
+		exit(1);
+	}
+
+	FILE *log = NULL;
+	struct TPMContext tpm;
+
+	if((log = fopen(argv[1], "r") ) != NULL) {
+		printf("open log:\t%s\n", argv[1]);
+		buildTPM(log, &tpm);	// build tpm
+		fclose(log);
+	} else {
+		fprintf(stderr, "error open log:\t%s\n", argv[1]);
+		exit(1);
+	}
+
 	return 0;
 }
